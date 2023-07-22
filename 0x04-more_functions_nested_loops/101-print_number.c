a70c4694cd6f1a35a4889f560151191b4242dcc2
MohamedOmAr963 <mohamed._omar@icloud.com>
2023-07-21 17:41:07 -0700

    ALX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
- */
+ * print_number - print some numbers
+ * @n: value of num
+*/
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int m  = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		m = -m;
 	}
-
-	if (n1 / 10)
+	if ((m / 10) > 0)
 	{
-		print_number(n1 / 10);
+		print_number(m / 10);
 	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar((m % 10) + 48);
 }
